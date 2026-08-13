a4b7316e9ab110c9fdf30f7df8c7378b1b38d85d
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-13 09:53:14 +0300

    add types for auth handler's props

---
+++ types/auth.ts
@@ -0,0 +1,19 @@
+type AuthCredentials = {
+  email: string
+  password: string
+}
+
+export type CallbackURLs = {
+	callbackURL: "/dashboard"
+}
+
+export type SignInProps = AuthCredentials & CallbackURLs;
+
+export type SignUpProps = {
+	name: string,
+	image?: string,
+} & AuthCredentials & CallbackURLs
+
+export type SocialProviders = {
+	provider: "github" | "google"
+}
