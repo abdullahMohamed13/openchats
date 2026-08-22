87ad69479a88538fec7d9638c15699b8018c990d
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-22 22:23:12 +0300

    add CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import CTABannerSection from "@/components/sections/CTABanner";
 import HeroSection from "@/components/sections/Hero";
+import HowItWorks from "@/components/sections/HowItWorks";
+import ProblemSection from "@/components/sections/Problem";
 
 export default function Home() {
 	return <div className="h-full">
 		<HeroSection />
+		<HowItWorks />
+		<ProblemSection />
+		<CTABannerSection />
 	</div>
 }
