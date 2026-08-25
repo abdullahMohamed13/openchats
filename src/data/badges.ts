8799ea61d5bf081cef605e69884c61ba4bba3806
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-25 11:37:21 +0300

    feat(onboarding): add personal choices icons/avatars for users to choose from

---
+++ src/data/badges.ts
@@ -0,0 +1,49 @@
+import type { StaticImageData } from "next/image";
+
+import bomb from "@/assets/icons/badges/bomb.webp";
+import book from "@/assets/icons/badges/book.webp";
+import chocolate from "@/assets/icons/badges/chocolate.webp";
+import coffeeCup from "@/assets/icons/badges/coffee-cup.webp";
+import crown from "@/assets/icons/badges/crown.webp";
+import donut from "@/assets/icons/badges/donut.webp";
+import falchion from "@/assets/icons/badges/falchion.webp";
+import flamingFire from "@/assets/icons/badges/flaming-fire.webp";
+import flash from "@/assets/icons/badges/flash.webp";
+import frog from "@/assets/icons/badges/frog.webp";
+import goldenCup from "@/assets/icons/badges/golden-cup.webp";
+import headset from "@/assets/icons/badges/headset.webp";
+import humanSkull from "@/assets/icons/badges/human-skull.webp";
+import iceCream from "@/assets/icons/badges/ice-cream.webp";
+import industryHelmet from "@/assets/icons/badges/industry-helmet.webp";
+import knightShield from "@/assets/icons/badges/knight-shield.webp";
+import orange from "@/assets/icons/badges/orange.webp";
+import pinkFlamingo from "@/assets/icons/badges/pink-flamingo.webp";
+import retroPhone from "@/assets/icons/badges/retro-phone.webp";
+import tv from "@/assets/icons/badges/tv.webp";
+import waterBottle from "@/assets/icons/badges/water-bottle.webp";
+import yellowStar from "@/assets/icons/badges/yellow-star.webp";
+
+export const BADGES: StaticImageData[] = [
+	bomb,
+	book,
+	chocolate,
+	coffeeCup,
+	crown,
+	donut,
+	falchion,
+	flamingFire,
+	flash,
+	frog,
+	goldenCup,
+	headset,
+	humanSkull,
+	iceCream,
+	industryHelmet,
+	knightShield,
+	orange,
+	pinkFlamingo,
+	retroPhone,
+	tv,
+	waterBottle,
+	yellowStar,
+];
