5e0229f87577c6da320f2758183198d53c5bdd05
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-27 22:40:04 +0300

    create auth UI

--- app/(auth)/signup/signup-form.tsx
+++ app/(auth)/signup/signup-form.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { authClient } from "@/lib/auth-client";
-import { handleSignup } from "@/lib/sign-up";
-
-const email = "abdllahahabm@gmail.com"
-const password = '11111111'
-const name = 'Abdallah Aziz 2'
-
-// const { data: session, isPending } = authClient.useSession();  // reactive
+import AuthSwitch from "@/components/ui/auth-switch";
 
 export default function SignUpForm() {
-	return (
-		<button className="bg-white p-4 text-black cursor-pointer"
-			onClick={() => handleSignup({ email, password, name, callbackURL: "/onboarding" })}
-		>
-			Sign Up
-		</button>
-	)
+  return <AuthSwitch initialMode="sign-up" />;
 }
